6ac8ab9480d3e2efab58b6db28eb51608473b029
samater <timacade433@gmail.com>
2023-02-04 10:53:11 +0300

    added base layout that other files will inheret

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,7 @@ tasks = ["foo", "bar", "baz"]
 def index(request):
     context ={"tasks":tasks}
     return render(request, "tasks/index.html", context)
+
+
+def add(request):
+    return render(request, "tasks/add.html")
