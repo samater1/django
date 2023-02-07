ff0caf9d35633dc33b2e065a78bbd5a21a65bde0
samater <timacade433@gmail.com>
2023-02-07 11:14:53 +0300

    configured sessions

--- tasks/views.py
+++ tasks/views.py
@@ -2,16 +2,14 @@ from django import forms
 from django.shortcuts import render, redirect
 
 # Create your views here.
-
-tasks = []
-names = []
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="Task")
-    name = forms.CharField(label="names")
 
 
 def index(request):
-    context = {"names": names,"tasks":tasks}
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    context = {"tasks":request.session["tasks"]}
     return render(request, "tasks/index.html", context)
 
 
@@ -20,9 +18,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
-            name = form.cleaned_data["name"]
-            names.append(name)
+            request.session["tasks"] += [task]
             return redirect("tasks:index")
         else:
             return render(request, "tasks/add.html", {"form":form})
