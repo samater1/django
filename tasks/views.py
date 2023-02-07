cf070e2cf7704ab1656ee5f4f71a191253aca7c4
samater <timacade433@gmail.com>
2023-02-07 10:27:30 +0300

    added django form to submit data

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
+names = []
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Task")
+    name = forms.CharField(label="names")
+
 
 def index(request):
-    context ={"tasks":tasks}
+    context = {"names": names,"tasks":tasks}
     return render(request, "tasks/index.html", context)
 
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            name = form.cleaned_data["name"]
+            names.append(name)
+            return redirect("tasks:index")
+        else:
+            return render(request, "tasks/add.html", {"form":form})
+            
+    return render(request, "tasks/add.html", {"form":NewTaskForm})
